420c19a47c881dcb6880be5edf6af98f4210da72
starb520 <97459015+starb520@users.noreply.github.com>
2023-01-16 15:17:46 -0700

    Got the local host running.

--- server.js
+++ server.js
@@ -6,13 +6,28 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
+
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout")
+
 
 /* ***********************
  * Routes
  *************************/
 app.use(require("./routes/static"))
+
+// Index route
+app.get("/", function(req, res){
+  res.render("index", {title: "Main"})
+})
 
 /* ***********************
  * Local Server Information
