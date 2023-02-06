98f747284b7b23c15a5d8c74b60269b6525dcba5
starb520 <97459015+starb520@users.noreply.github.com>
2023-02-06 09:42:17 -0700

    changed pool object

--- database/index.js
+++ database/index.js
@@ -8,9 +8,9 @@ require("dotenv").config()
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false,
-    },
+    // ssl: {
+    //     rejectUnauthorized: false,
+    // },
 })
 
 module.exports = pool
