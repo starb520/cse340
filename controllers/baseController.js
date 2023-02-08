a3e4289e7251bd7cb82da40c9c32f81d3295d6e8
starb520 <97459015+starb520@users.noreply.github.com>
2023-02-08 13:43:47 -0700

    updating file paths

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -1,4 +1,4 @@
-const utilities = require("../utilities")
+const utilities = require("../utilities/")
 const baseController = {}
 
 baseController.buildHome = async function (req, res) {
--- models/inventory-model.js
+++ models/inventory-model.js
@@ -1,4 +1,4 @@
-const pool = require("../database")
+const pool = require("../database/")
 
 async function getClassifications(){
     return await pool.query("SELECT * FROM public.classification ORDER BY classification_name");
