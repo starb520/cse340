5c4a31dcaed69b93c9f16c52b84ab10d6aa79703
starb520 <97459015+starb520@users.noreply.github.com>
2023-02-16 14:10:52 -0700

    trying to save registration to database

--- models/account-model.js
+++ models/account-model.js
@@ -11,7 +11,7 @@ async function registerClient (
 ) {
     try {
         const sql = 
-            "INSERT INTO client (client_firstname, client_lastname, client_email, client_password, client_type) VALUES ($1, $2,$3, $4, 'Client')RETURNING *"
+            "INSERT INTO client (client_firstname, client_lastname, client_email, client_password, client_type) VALUES ($1, $2,$3, $4, 'Client') RETURNING *"
         return await pool.query(sql, [
             client_firstname,
             client_lastname,
